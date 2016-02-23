1b648a390065b6dcca088a70127d99ded332c435
Kalin Karev <karev.kalin@gmail.com>
2016-02-23 19:44:57 +0200

    Homework maze done

--- 11a/15/02/maze.cc
+++ 11a/15/02/maze.cc
@@ -5,8 +5,6 @@
 
 using namespace std;
 
-class BoardError{};
-
 enum Direction {
     UP = 1,
     RIGHT = 1 << 1,
@@ -14,7 +12,9 @@ enum Direction {
     LEFT = 1 << 3
 };
 
-class Cell : public Shape {
+class MazeError{};
+
+class Cell {
     friend class Board;
 
     int row_, col_;
@@ -43,21 +43,9 @@ class Cell : public Shape {
     
     public:
     
-    Cell(int row, int col, int len) : visited_(false), row_(row), col_(col), walls_(UP | RIGHT | DOWN | LEFT),
+    Cell(int row, int col) : visited_(false), row_(row), col_(col), walls_(UP | RIGHT | DOWN | LEFT),
     up_(0), right_(0), down_(0), left_(0) {}
-
-    bool visited() const {
-        return visited_;
-    }
-
-    void set_visited() {
-        visited_ = true;
-    }
     
-    bool has_wall(Direction dir) const {
-        return walls_ & dir;
-    }
-
     void drill(Direction dir) {
         //dir = 1000
         //~dir = 0111
@@ -65,44 +53,58 @@ class Cell : public Shape {
         //wall_ & ~dir = 0111
         walls_ &= ~dir;
     }
-
+    
     void drill_to(Cell* n) {
         if(n == up()) {
             drill(UP);
             n->drill(DOWN);
+        } else if(n==right()) {
+            drill(RIGHT);
+            n->drill(LEFT);
         } else if(n==down()) {
             drill(DOWN);
             n->drill(UP);
         } else if(n==left()) {
             drill(LEFT);
             n->drill(RIGHT);
-        } else if(n==right()) {
-            drill(RIGHT);
-            n->drill(LEFT);
         } else {
-            throw BoardError();
+            throw MazeError();
         }
     }
 
-    void draw(int len = 10) {
-        Path path;
-        path.add(Path::Option("M", col_*len));
-        path.add(Path::Option("", row_*len));
-        path.add(Path::Option(has_wall(DOWN) ? "l" : "m", len));
-        path.add(Path::Option("", 0));
+    bool has_wall(Direction dir) const {
+        // walls_ = 1011
+        // dir =    1000 => 1000
+        // dir =    0010 => 0010
+        // dir =    0100 => 0000
+        return walls_ & dir;
+    }
 
-        path.add(Path::Option(has_wall(RIGHT) ? "l" : "m", 0));
-        path.add(Path::Option("", len));
+    /*
+    void draw(int length) const {
+        cout << row_*length << " " << col_*length << " moveto" << endl;
+        cout << length << " " << 0 << (has_wall(DOWN) ? " rlineto" : " rmoveto") << endl;
+        cout << 0 << " " << length << (has_wall(RIGHT) ? " rlineto" : " rmoveto")  << endl;
+        cout << -length << " " << 0 << (has_wall(UP) ? " rlineto" : " rmoveto") << endl;
+        cout << 0 << " " << -length << (has_wall(LEFT) ? " rlineto" : " rmoveto") << endl;
+    }
+    */
 
-        path.add(Path::Option(has_wall(UP) ? "l" : "m", -len));
-        path.add(Path::Option("", 0));
+    void draw(Path* path, int len = 10) const {
+        path->add(Path::Option("M", col_*len));
+        path->add(Path::Option("", row_*len));
 
-        path.add(Path::Option(has_wall(LEFT) ? "l" : "m", 0));
-        path.add(Path::Option("", -len));
-        
-        path.set_property("stroke", "green");
-        path.set_property("fill", "none");
-        path.draw();
+        path->add(Path::Option((has_wall(DOWN) ? "l" : "m"), len));
+        path->add(Path::Option("", 0));
+
+        path->add(Path::Option((has_wall(RIGHT) ? "l" : "m"), 0));
+        path->add(Path::Option("", len));
+
+        path->add(Path::Option((has_wall(UP) ? "l" : "m"), -len));
+        path->add(Path::Option("", 0));
+
+        path->add(Path::Option((has_wall(LEFT) ? "l" : "m"), 0));
+        path->add(Path::Option("", -len));
     }
 
     Cell* up() {
@@ -118,26 +120,38 @@ class Cell : public Shape {
         return right_;
     }
 
-    vector<Cell*> get_unvisited_neighbours() {
-        vector<Cell*> result;
-        if( up() != 0 && ! up()->visited() )
-            result.push_back(up());
-        if( right() != 0 && ! right()->visited() )
-            result.push_back(right());
-        if( down() != 0 && ! down()->visited() ) 
-            result.push_back(down());
-        if( left() != 0 && ! left()->visited() )
-            result.push_back(left());
-        return result;
+    void visit() {
+        visited_ = true;
+    }
+    
+    bool is_visited() {
+        return visited_; 
     }
     
     bool has_unvisited_neighbours() {
         return !get_unvisited_neighbours().empty();
     }
-    
+
+    vector<Cell*> get_unvisited_neighbours() {
+        vector<Cell*> result;
+        if( up() != 0 && ! up()->is_visited() ){
+            result.push_back(up());
+        }
+        if( right() != 0 && ! right()->is_visited() ) {
+            result.push_back(right());
+        }
+        if( down() != 0 && ! down()->is_visited() ) { 
+            result.push_back(down());
+        }
+        if( left() != 0 && ! left()->is_visited() ) {
+            result.push_back(left());
+        }
+        return result;
+    }
+        
     Cell* get_random_unvisited_neighbour() {
         if(!has_unvisited_neighbours()) {
-            throw BoardError();
+            throw MazeError();
         }
         vector<Cell*> neighbours = get_unvisited_neighbours();
         int r = rand() % neighbours.size();
@@ -147,15 +161,14 @@ class Cell : public Shape {
 
 class Board {
     vector<Cell> cells_;
-    int rows_, cols_, size_;
-    Path path;
+    int rows_, cols_;
     
     public:
     
-    Board(int rows, int cols, int size) : rows_(rows), cols_(cols), size_(size) {
+    Board(int rows, int cols) : rows_(rows), cols_(cols) {
         for (int i = 0; i < rows_; i++) {
             for(int j = 0; j < cols_; j++) {
-                cells_.push_back(Cell(i, j, size_));
+                cells_.push_back(Cell(i, j));
             }
         }
         
@@ -178,44 +191,63 @@ class Board {
         return cells_[i * cols_ + j];
     }
     
-    void draw() {
-		//cout << "newpath" << endl;
+    /*
+    void draw(int size = 10) const {
+		cout << "newpath" << endl;
 
-        Canvas canvas(size_ * rows_, size_ * cols_);
-
-		for(vector<Cell>::iterator it = cells_.begin(); it!=cells_.end(); ++it) {
+		for(vector<Cell>::const_iterator it = cells_.begin();
+			it!=cells_.end(); ++it) {
 		
-			//(*it).draw(size);	
+			(*it).draw(size);	
+		}
 		
-            canvas.add(&(*it));
-
-
-        }
+		cout << "stroke" << endl;
+		cout << "showpage" << endl;
 		
-		//cout << "stroke" << endl;
-		//cout << "showpage" << endl;
-		
-        canvas.draw();
-
 	}
+    */
+
+    void draw(int w, int h, Path* path) const {
+        for(vector<Cell>::const_iterator it = cells_.begin(); it != cells_.end(); ++it) {
+            (*it).draw(path);
+        }
+    }
 };
 
-void generate(Board& b, Cell& c) {
-    c.set_visited();
-    while(c.has_unvisited_neighbours()) {
-        Cell* next=c.get_random_unvisited_neighbour();
-        //next->set_visited();
-        c.drill_to(next);
-        generate(b, *next);
-    }
-}
+class Maze {
+    int w_, h_;
+    Path path_;
 
+    void generate_maze(Board& board, Cell& start) {
+        start.visit();
+        while (start.has_unvisited_neighbours()) {
+            Cell* next = start.get_random_unvisited_neighbour();
+            start.drill_to(next);
+            generate_maze(board, *next);
+        }
+    }
+
+public:
+
+    Maze(int w, int h): w_(w), h_(h) {
+        path_.add(Path::Option("M", 50));
+        path_.add(Path::Option("", 50));
+        path_.set_property("stroke", "green").set_property("fill", "none").set_property("stroke-width", "2");
+    }
+
+    void draw() {
+        Canvas canvas(w_, h_);
+        Board board(w_, h_);
+        Cell& start = board.at(0, 0);
+        generate_maze(board, start);
+        board.draw(w_, h_, &path_);
+        canvas.add(new Path(path_));
+        canvas.draw();
+    }
+};
 
 int main() {
-    Board b(30, 30, 40);
-    Cell& c  = b.at(0, 0);
-    generate(b, c);
-    b.draw();
-
+    Maze maze (300, 300);
+    maze.draw();
     return 0;
 }
