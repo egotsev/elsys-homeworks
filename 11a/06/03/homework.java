b172965fac85c7442cf35640b5899c43973a689c
vicky98 <vikiga98@gmail.com>
2016-02-29 21:31:19 +0200

    Update homework.java

--- 11a/06/03/homework.java
+++ 11a/06/03/homework.java
@@ -1,14 +1,15 @@
-import java.util.HashMap;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 public class homeworkNo3 
 {
 	public static void main(String[] args) 
 	{
-		HashMap<String, String> properties = new HashMap<String, String>();
+		TreeMap<String, String> properties = new TreeMap<String, String>();
 		String property;
 		int i = 0;
 		
-		while (i < (args.length -1))
+		while (i <= (args.length -1))
 		{
 			property = System.getProperty(args[i]);
 					
@@ -20,12 +21,22 @@ public class homeworkNo3
 			i++;
 		}
 		
-		if (args[args.length - 1] == "up")
+		if (args[args.length - 1].equals("up") || !args[args.length - 1].equals("down"))
+		{
+			NavigableMap<String, String> properties2 = properties.descendingMap();
+			
+			for (String element : properties2.keySet())
+			{
+				System.out.println(properties2.get(element));
+			}
+		} 
+		
+		if (args[args.length - 1].equals("down"))
 		{
 			for (String element : properties.keySet())
 			{
 				System.out.println(properties.get(element));
-			};
+			}
 		}
 	}
 }
