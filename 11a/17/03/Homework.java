aec80aff0b5e2ad534aba6dbbb788627b11e62eb
Juke <marto_dimitrov@mail.bg>
2016-02-29 13:50:52 +0200

    :tractor: Homework(System functions) :tractor:

---
+++ 11a/17/03/Homework.java
@@ -0,0 +1,29 @@
+import java.util.Vector;
+public class Property
+{
+	public static void main(String[] args)
+	{
+		Vector<String> vector = new Vector<String>();
+		for(int i = 0 ; i < args.length ; i++)
+		{
+			String property = System.getProperty(args[i]);
+			if (property != null)
+			{
+				vector.add(property);
+			}
+		}
+		String lastProperty = args[args.length - 1];
+		boolean asc = lastProperty.compare("down") != 0;
+		for(int i = 0; i < vector.size(); i++)
+		{
+			if(asc){
+				String out = vector.elementAt(i);
+			}
+			else
+			{
+					vector.elementAt((vector.size() - i) - 1);
+			}
+			System.out.println(out);
+		}
+	}
+}
