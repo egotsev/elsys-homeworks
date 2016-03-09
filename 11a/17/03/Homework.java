ace376befd8be6fa40cb142553729fecdcf0ed8b
Juke <marto_dimitrov@mail.bg>
2016-03-09 17:52:49 +0200

    Fixed

--- 11a/17/03/Homework.java
+++ 11a/17/03/Homework.java
@@ -3,24 +3,20 @@ public class Property
 {
 	public static void main(String[] args)
 	{
-		Vector<String> vector = new Vector<String>();
-		for(int i = 0 ; i < args.length ; i++)
-		{
+		Vector<String> vector = new Vector<>();
+		for(int i = 0 ; i < args.length ; i++){
 			String property = System.getProperty(args[i]);
-			if (property != null)
-			{
+			if (property != null){
 				vector.add(property);
 			}
 		}
 		String lastProperty = args[args.length - 1];
 		boolean asc = lastProperty.compare("down") != 0;
-		for(int i = 0; i < vector.size(); i++)
-		{
+		for(int i = 0; i < vector.size(); i++){
 			if(asc){
 				String out = vector.elementAt(i);
 			}
-			else
-			{
+			else{
 					vector.elementAt((vector.size() - i) - 1);
 			}
 			System.out.println(out);
