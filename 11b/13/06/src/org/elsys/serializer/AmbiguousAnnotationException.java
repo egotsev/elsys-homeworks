5f78c898e9df86ed9f0009b077fdc40cafa04031
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2016-04-03 18:35:17 +0300

    Implement some JSON serializer features

---
+++ 11b/13/06/src/org/elsys/serializer/AmbiguousAnnotationException.java
@@ -0,0 +1,7 @@
+package org.elsys.serializer;
+
+public class AmbiguousAnnotationException extends RuntimeException {
+	public AmbiguousAnnotationException(String message) {
+		super(message);
+	}
+}
