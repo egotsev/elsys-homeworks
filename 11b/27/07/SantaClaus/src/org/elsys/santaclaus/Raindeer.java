49f21ec5a8134b314b1ee23959c4478417da6145
radostin-angelov <angelovradostin@gmail.com>
2016-04-26 13:44:14 +0300

    santa claus problem added

---
+++ 11b/27/07/SantaClaus/src/org/elsys/santaclaus/Raindeer.java
@@ -0,0 +1,13 @@
+package org.elsys.santaclaus;
+
+public class Raindeer {
+	private int id;
+	
+	public Raindeer(int id) {
+		this.id = id;
+	}
+	
+	public void getHitched() {
+		System.out.println("Raindeer got hitched!");
+	}
+}
